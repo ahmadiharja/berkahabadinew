08e02e5a2b1d13b12b181fe208205e63c1f281cf
ahmadiharja <mozfaith@gmail.com>
2025-03-24 06:37:27 +0700

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,11 @@
 import { Sun, Moon } from "lucide-react";
+import berkahabadilogo from "../../assets/images/logoptberkahabadidarkcircle.png";
 
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-[var(--bg-primary)]/80 backdrop-blur-md shadow-md py-2">
       <img 
-        src="/src/assets/images/logoptberkahabadidarkcircle.png"
+        src={berkahabadilogo}
         alt="PT Berkah Abadi"
         className="w-10 h-10 rounded-full mx-auto"
       />
